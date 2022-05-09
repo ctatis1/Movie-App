9b431228d22a30705c54700f72c256d9a35d17c7
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-05-08 22:17:31 -0500

    sumador y restador con useState

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import './App.css';
 import {useState} from 'react';
 
-const Person = (props) =>{
-  return(
-    <>    
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-
-  );
-}
-
 const App =() => {
-  const name = 'Camilo';
-  const isnameShowing = true;
+  //concepto de array destructuring
+  //'use' hace referencia a los Hooks
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello {isnameShowing ? name:'someone'}</h1>
-      {name ? (
-        <> {/**react fragment
-         * sirve pa poner varios divs (por decir una función en un mismo espacio)
-         */}
-          test
-        </>
-      ):(
-        <>
-          <h1>test</h1>  
-          <h2>Theres no name</h2>      
-        </>
-      )}
-      <Person />
-      <Person 
-        name={'Maria'} 
-        lastName={'Torres'} 
-        age={56}
-      />
+      <button onClick={()=> setCounter(prevCount=> prevCount -1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=> setCounter(prevCount=> prevCount +1)}>+</button>
     </div>
   );
 }
