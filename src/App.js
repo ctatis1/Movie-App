3fdc9940650209606890caac718d258a0ceb0f98
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-05-09 16:58:43 -0500

    Interfaz con el mockup de una movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { useEffect } from "react";
+import './App.css';
+import SearchIcon from './search.svg';
 
 //API key: cb4d33e7
 
 const API_URL = 'http://www.omdbapi.com?apikey=cb4d33e7';
+
+//movie de prueba
+const movie1 = {
+    "Title": "The Batman",
+    "Year": "2022",
+    "imdbID": "tt1877830",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
+};
 
 const App = () => {
     //se quiere buscar title dentro de searchMovies
@@ -21,7 +32,41 @@ const App = () => {
     }, []);
 
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>Movies</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    //solo aparece Superman y no deja modificarlo pa eso está el onChange
+                    value='Superman'
+                    onChange={()=>{}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt='Search'
+                    //el icono funciona también como botón por eso se debe coloca un onClick
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img
+                            src={movie1.Poster !== 'N/A' ? movie1.Poster: 'http://via.placerholder.com/400'} alt={movie1.Title}
+                        />
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
