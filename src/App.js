71bc885ac33edaa009254be1e257b8d9690cfc30
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-05-10 12:48:41 -0500

    Movies desplegadas según el elemento asignado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
-import './App.css';
+import { useEffect, useState } from "react";
 import SearchIcon from './search.svg';
-
+import MovieCard from "./MovieCard";
+import './App.css';
 //API key: cb4d33e7
 
 const API_URL = 'http://www.omdbapi.com?apikey=cb4d33e7';
@@ -17,6 +17,12 @@ const movie1 = {
 };
 
 const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    //cargar los datos de la API solo una vez cuando cargue la página
+    useEffect(()=>{
+        searchMovies('Batman');
+    }, []);
     //se quiere buscar title dentro de searchMovies
     const searchMovies = async (title) =>{
         //hará el llamado a la API
@@ -24,12 +30,11 @@ const App = () => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();  
         //data devuelve varios objetos pero solo se necesita Search
-        console.log(data.Search) 
+        //console.log(data.Search) 
+        //definir todas las movies mediante el data.Search que retorna todas las movies
+        setMovies(data.Search)
     }
-    //cargar los datos de la API solo una vez cuando cargue la página
-    useEffect(()=>{
-        searchMovies('Batman');
-    }, []);
+    
 
     return(
         <div className="app">
@@ -50,8 +55,27 @@ const App = () => {
                 />
             </div>
 
-            <div className="container">
-            </div>
+            {/**si el array de movies está vacío significa que no encontró ninguna movie de la búsqueda */}
+            {/* A ternary operator. It is a conditional operator that takes three operands. The first
+            operand is a comparison argument, the second one is the result upon a true comparison,
+    and the third one is the result upon a false comparison. */}
+            {
+               movies?.length > 0 
+               ? (
+                    <div className="container">
+                        {/* Passing the movie1 object as a prop to the MovieCard component. */}
+                        {movies.map((movie)=>(
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+               ):(
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+               )
+            }
+
+            
         </div>
     );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 //usar object destructuring para evitar colocar props. todo el timepo
-const MovieCard = ({movie1}) => {
+const MovieCard = ({movie}) => {
     return (
         <div className="movie">
             <div>
-                <p>{movie1.Year}</p>
+                <p>{movie.Year}</p>
             </div>
             <div>
                 <img
-                    src={movie1.Poster !== 'N/A' ? movie1.Poster: 'http://via.placerholder.com/400'} alt={movie1.Title}
+                    src={movie.Poster !== 'N/A' ? movie.Poster: 'http://via.placerholder.com/400'} alt={movie.Title}
                 />
             </div>
             <div>
-                <span>{movie1.Type}</span>
-                <h3>{movie1.Title}</h3>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
             </div>
         </div>
     );
